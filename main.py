16f038450e73a16c2ac0a6e1229bc769dd98f566
kirybakov <114290407+kirybakov@users.noreply.github.com>
2023-01-10 13:19:19 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+import math
+
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+class Roots:
+
+    def Discriminant(self, a, b, c):
+        Disc = b * b - 4 * a * c
+        return Disc
+
+    def Oneroot(self, a, b):
+        root1 = -b / (2 * a)
+        return root1
+
+    def Tworoots(self, a, b, D):
+        root1 = (-b + math.sqrt(D, 0.5) / (2 * a)
+        root2 = (-b - math.sqrt(D, 0.5) / (2 * a)
+        return sorted([root1, root2])
---
+++ test.py
@@ -0,0 +1,29 @@
+import unittest
+from main import Roots
+
+
+class TestRoots(unittest.TestCase):
+
+    def setUp(self):
+        self.Roots = Roots()
+
+    def test_GoodDisc(self):
+        self.assertEqual(self.Roots.Discriminant(1, -8, 12), 16)
+
+    def test_NullDisc(self):
+        self.assertEqual(self.Roots.Discriminant(1, -6, 9), 0)
+
+    def test_NotExistDisc(self):
+        self.assertEqual(self.Roots.Discriminant(5, 3, 7), -131)
+
+    def test_OneRoot(self):
+        self.assertEqual(self.Roots.Oneroot(1, 12), 0.0)
+
+    def test_TwoRoots(self):
+        self.assertEqual(self.Roots.Tworoots(-1, -2, 64), [-5, 3])
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
