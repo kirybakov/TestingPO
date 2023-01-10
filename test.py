ed5290ec5745934d848060fc7dc4024b7943d8de
kirybakov <114290407+kirybakov@users.noreply.github.com>
2023-01-10 13:45:13 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import unittest
+""" import unittest
 from RootsMain import Roots
 
 
@@ -23,6 +23,32 @@ class TestRoots(unittest.TestCase):
         self.assertEqual(self.Roots.Tworoots(-1, -2, 64), [-5, 3])
 
 
+if __name__ == "__main__":
+    unittest.main() """
+
+import unittest
+from rootsmain import Klass 
+
+class TestEquation(unittest.TestCase):
+
+    def setUp(self):
+        self.Klass = Klass()
+
+    def test_PositiveDiscriminant(self):
+        self.assertEqual(self.Klass.discriminant(3, 7, -6), 121)
+    
+    def test_ZeroDiscriminant(self):
+        self.assertEqual(self.Klass.discriminant(4, 4, 1), 0)
+
+    def test_NegativeDiscriminant(self):
+        self.assertEqual(self.Klass.discriminant(2, 1, 1), -7)
+        
+    def test_Squareroot(self):
+        self.assertEqual(self.Klass.squareroot(1, 0), 0.0)
+        
+    def test_Squareroots(self):
+        self.assertEqual(self.Klass.squareroots(1, 2, 16), [-3.0, 1])
+  
 if __name__ == "__main__":
     unittest.main()
 
